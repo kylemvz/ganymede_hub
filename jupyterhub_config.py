d16b6b6d8d89497630d24c87af61961753aa36f3
kylemvz <kylez@lab41.org>
2016-03-31 15:15:06 -0700

    Added username_map as a configuration variable (maps GitHub username to system usernames. Using ast.literal_eval to create boolean value and dict.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,7 +1,10 @@
 import os
+import ast
 
 c.JupyterHub.spawner_class = 'ganymede_hub.GanymedeSpawner.GanymedeSpawner'
 c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
+
+c.GitHubOAuthenticator.username_map = ast.literal_eval(os.environ["USERNAMES_MAP"])
 
 c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 c.GitHubOAuthenticator.client_id = os.environ['GITHUB_CLIENT_ID']
@@ -10,4 +13,4 @@ c.GitHubOAuthenticator.client_secret = os.environ['GITHUB_CLIENT_SECRET']
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
 
 c.DockerSpawner.hub_ip_connect = os.environ['HUB_IP_CONNECT']
-c.DockerSpawner.use_internal_ip = (os.environ['USE_INTERNAL_IP'] == "True")
+c.DockerSpawner.use_internal_ip = ast.literal_eval(os.environ['USE_INTERNAL_IP'])
